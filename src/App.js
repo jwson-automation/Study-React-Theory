281b6601a0c025b336b670eccd2518bfb40dd620
so8ju <81137045+so8ju@users.noreply.github.com>
2022-07-07 14:11:18 +0900

    study about onClick event

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import './App.css';
 import {useState} from 'react';
 
 // ---------------------------------------------------
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>}
-
-
-
-// ---------------------------------------------------
 function Header(props){
   return <header>
   <h1><a href="/" onClick={(event)=>{
@@ -18,53 +9,68 @@ function Header(props){
     props.onChangeMode();
   }}>{props.title}</a></h1>
  </header>}
-
-
-
+ 
 // ---------------------------------------------------
 function Nav(props){
     const lis = [] 
   for(let i=0; i<props.topics.length; i++)
   {let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a href="{'/read/'+t.id}" onClick={event=>{
-      event.preventDefault();
-      // props.onChangeMode(Number(event.target.id));
-      // console.log(event.target.id);
-      // props.onChangeMode(Number(props.id));
-      // console.log(props.id); 왜 안될까
-      props.onChangeMode(Number(t.id));
-      console.log(t.id);
-      }} id={t.id}>{t.title}</a></li>)
+    lis.push(
+    <li id={t.id} key={t.id}><a href="{'/read/'+t.id}" 
+      onClick={event=>{event.preventDefault();
+                props.onChangeMode(Number(t.id));
+                 // props.onChangeMode(Number(event.target.id));
+                }}>{t.title}</a>
+                </li>)
   }
   return <nav>
             <ol>
               {lis}
             </ol>
-  </nav>}
- 
+  </nav>
+  }
+// ---------------------------------------------------
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>}
+// ---------------------------------------------------
+function Create(props){
+  return <article>
+  <h2>Create</h2>
+  <form onSubmit={event=>{
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    props.onCreate(title, body);
+  }}>
+    <p><input type ="text" name ="title" placeholder='title'/></p>
+    <p><input type ="text" name ="body" placeholder='body' /></p>
+    <p><input type="submit" value ="create"></input></p>
 
-
+  </form>
+  </article>
+}
 
 // Main-----------------------------------------------
 function App() {
-  const topics = [
+  // topics
+  const [topics, setTopics] = useState([
     {id:1, title:'html' , body:'html is...'},
     {id:2, title:'css' , body:'css is...'},
-    {id:3, title:'js' , body:'js is...'}
-  ]
-
-  // const _mode = useState('WELCOME');
-  // const mode = _mode[0];
-  // const setMode = _mode[1]
+    {id:3, title:'js' , body:'js is...'}])
+  // use State
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-
+  const [nextId, setNextId] = useState(4);
+  
+  // content
   let content =null;
 
-  if (mode==='WELCOME'){
+  if (mode==='WELCOME'){//----------------------------------------------------------WELCOME 모드
     content = <Article title="Welcome" body="Hello,React"></Article>
-  }else if(mode==='READ'){
+  }else if(mode==='READ'){//----------------------------------------------------------READ 모드
     let title, body = null;
     for(let i=0; i<topics.length; i++){
       // console.log(topics[i].id, id);
@@ -74,6 +80,21 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  }else if(mode==='CREATE'){ //----------------------------------------------------------CREATE 모드
+    content = <Create onCreate={(_title, _body)=>{
+        const newTopic = {id:nextId, title:_title, body:_body}
+        // topics.push(newTopics)
+        // setTopics(topics); 객체, 배열은 이 방법이 아닌 복제해서 넣는 방법을 사용해야 정상 동작함
+        const newTopics = [...topics]
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+
+        
+        setMode('READ');        
+        
+        setId(nextId);
+        setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -85,7 +106,10 @@ function App() {
        setId(_id);
        }}></Nav>
        {content}
-
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+        }}>Create</a>
     </div>
   );}
 
